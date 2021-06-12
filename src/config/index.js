ccd0a12df1119388483a9bdf75c3c20b055b1c53
hsimao <e087754958@gmail.com>
2021-06-12 16:47:33 +0800

    feat: config setting

---
+++ src/config/index.js
@@ -0,0 +1,23 @@
+const env = import.meta.env.MODE || 'production'
+const EnvConfig = {
+  development: {
+    baseApi: '/',
+    mockApi:
+      'https://www.fastmock.site/mock/5a43111788188a7ff3b1d28431be3016/api'
+  },
+  test: {
+    baseApi: '//test.futurefe.com/api',
+    mockApi:
+      'https://www.fastmock.site/mock/5a43111788188a7ff3b1d28431be3016/api'
+  },
+  production: {
+    baseApi: '//futurefe.com/api',
+    mockApi:
+      'https://www.fastmock.site/mock/5a43111788188a7ff3b1d28431be3016/api'
+  }
+}
+export default {
+  env,
+  mock: true,
+  ...EnvConfig[env]
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
+import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
+import axios from 'axios'
+import config from './config'
+
+axios.get(config.mockApi + '/login').then((res) => {
+  console.log('api res', res)
+})
 
 console.log(import.meta.env)
 
-createApp(App).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(ElementPlus).mount('#app')
