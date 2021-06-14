8538fae08fc398b40be5e7fac9f1f0bf95112cce
hsimao <e087754958@gmail.com>
2021-06-14 22:42:39 +0800

    feat: welcome

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>Welcome</h1>
-  <router-link to="/login">去登入</router-link>
+  <div class="welcome">
+    <div class="content">
+      <h2 class="subtitle">歡迎體驗</h2>
+      <h1 class="title">通用後台管理系統</h1>
+      <div class="desc">- Vue3.0 + ElementPlus + Node + Mongo</div>
+    </div>
+    <div class="img"></div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +15,40 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.welcome {
+  height: 100%;
+
+  @include flexCenter;
+
+  .content {
+    position: relative;
+    bottom: 40px;
+
+    .subtitle {
+      color: $colorBlack;
+      font-size: 30px;
+      line-height: 42px;
+    }
+
+    .title {
+      color: $colorPrimary;
+      font-size: 40px;
+      line-height: 62px;
+    }
+
+    .desc {
+      color: $colorBlackLight;
+      text-align: right;
+      font-size: 14px;
+    }
+  }
+
+  .img {
+    margin-left: 100px;
+    width: 371px;
+    height: 438px;
+    background-image: url('@/assets/images/welcome.png');
+  }
+}
+</style>
